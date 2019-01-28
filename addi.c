92132e4be0b43861975f38dd95d98f90e131c601
mdasifrashid <asif.rashid@vvdntech.in>
2019-01-28 16:05:51 +0530

    first commit

---
+++ addi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void add(int x,int y);
+main()
+{
+add(4,5);
+}
+void add(int x,int y)
+{
+int z;
+z=x+y;
+printf("%d",z);
+}
---
+++ sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void sub(int x,int y);
+int main()
+{
+sub(5,2);
+}
+void sub(int x,int y)
+{
+int z;
+z=x-y;
+printf("%d",z);
+}
+
